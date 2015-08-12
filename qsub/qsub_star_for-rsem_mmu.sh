a28a9ad115a891cba11bdd2d13f8a62c2da71b31
David J. H. Shih <djh.shih@gmail.com>
2015-08-12 15:48:31 -0400

    Fixed error in qsub for star.

--- qsub/qsub_star_for-rsem_mmu.sh
+++ qsub/qsub_star_for-rsem_mmu.sh
@@ -9,7 +9,7 @@ scriptpath=$(dirname "$(readlink -f "$0")")
 rootpath=$scriptpath/..
 binpath=$rootpath/star
 inpattern='*.fastq.gz'
-script=$binpath/star_for-rsem_gencode_hsa.pbs
+script=$binpath/star_for-rsem_gencode_mmu.pbs
 
 indir=$(readlink -f $1)
 outdir=$(readlink -f $2)
@@ -22,8 +22,8 @@ for d in $(ls -1 $indir); do
 		mkdir -p $outdir/$d && cd $outdir/$d
 		for f in $indir/$d/$inpattern; do
 			if [ -f $f ]; then
-				if [ ! -s ".$fname.done" ]; then
-					fname=${f##*/}
+				fname=${f##*/}
+				if [ ! -s "$outdir/$d/.$fname.done" ]; then
 					grep "^#\!\|^#PBS" $script > $fname.job.pbs
 					echo "set -e" >> $fname.job.pbs
 					echo "cd $(pwd)" >> $fname.job.pbs
