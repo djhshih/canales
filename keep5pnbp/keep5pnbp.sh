b1a8cac0f139f96c5644deae061888986a60a512
David J. H. Shih <djh.shih@gmail.com>
2015-08-12 20:04:02 -0400

    Check if read cutting is feasible and ln input file otherwise.

--- keep5pnbp/keep5pnbp.sh
+++ keep5pnbp/keep5pnbp.sh
@@ -14,13 +14,22 @@ rootpath=$scriptpath/..
 
 targetlen=$3
 observedlen=$($rootpath/readlen/readlen.py $infile)
-cutlen=$(expr $observedlen - $targetlen)
+cutlen=$(( observedlen > targetlen ? observedlen - targetlen : 0 ))
 
 echo "target read length: $targetlen bp"
 echo "observed read length: $observedlen bp"
 echo "length to cut from 3' end: $cutlen bp"
 echo
 
-# in order to keep $targetlen from the 5' end of the reads,
-# cut $cutlen from the 3' end
-cutadapt --cut -$cutlen -o $outfile $infile
+if (( cutlen > 0 )); then
+	# in order to keep $targetlen bp from the 5' end of the reads,
+	# cut $cutlen from the 3' end
+	cutadapt --cut -$cutlen -o $outfile $infile
+else
+	echo "Reads are too short to cut."
+	echo "Creating symbolic link to original file in output directory."
+	outdir=${outfile%/*}
+	infname=${infile##*/}
+	ln -sv $infile $outdir/$infname
+fi
+
