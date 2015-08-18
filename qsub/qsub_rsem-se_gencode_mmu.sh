10b840d057094fb7400b828da16c02a2db15aac8
David J. H. Shih <djh.shih@gmail.com>
2015-08-18 17:53:07 -0400

    Added rsem.

---
+++ qsub/qsub_rsem-se_gencode_mmu.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+	echo "usage: script <in_dir> <out_dir>"
+	exit 1
+fi
+
+scriptpath=$(dirname "$(readlink -f "$0")")
+rootpath=$scriptpath/..
+binpath=$rootpath/rsem
+inpattern='*.toTranscriptome.out.bam'
+script=$binpath/rsem-se_gencode_mmu.pbs
+
+indir=$(readlink -f $1)
+outdir=$(readlink -f $2)
+
+mkdir -p $outdir && cd $outdir
+
+for d in $(ls -1 $indir); do
+	if [ -d "$indir/$d" ]; then
+		echo "Processing $indir/$d ..."
+		mkdir -p $outdir/$d && cd $outdir/$d
+		for f in $indir/$d/$inpattern; do
+			if [ -f $f ]; then
+				fname=${f##*/}
+				if [ ! -s "$outdir/$d/.$fname.done" ]; then
+					grep "^#\!\|^#PBS" $script > $fname.job.pbs
+					echo "set -e" >> $fname.job.pbs
+					echo "cd $(pwd)" >> $fname.job.pbs
+					echo "$script $f" >> $fname.job.pbs
+					echo "echo \$? > .$fname.done" >> $fname.job.pbs
+					qsub $fname.job.pbs
+				fi
+			fi
+		done
+	fi
+done
+
---
+++ rsem/rsem-se_gencode.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+if [ "$#" -ne 5 ]; then
+	echo "usage: script <bam> <species> <genome_version> <gencode_version> <nthreads>"
+	exit 1
+fi
+
+inbam=$1
+species=$2
+genome_version=$3
+gencode_version=$4
+nthreads=$5
+
+inpath=$(readlink -f $inbam)
+indir=${inpath%/*}
+sample_name=${indir##*/}
+# the parent directory is named with the sample name
+
+echo $sample_name
+
+# Generate expression table and additional BAM files
+
+# use the minimum allowable seed length to avoid discarding reads successfully
+# mapped by STAR
+$CANALES_CHROOT rsem-calculate-expression \
+	-p $nthreads \
+	--seed-length 5 \
+	--bam $inbam \
+	--output-genome-bam \
+	${CANALES_DATA_PATH}/rsem/gencode/${species}/release_${gencode_version}/${genome_version}_gencode-${gencode_version} \
+	$sample_name
+
+# Generate plots
+
+$CANALES_CHROOT rsem-bam2wig ${sample_name}.genome.sorted.bam ${sample_name}.genome.wig $sample_name
+$CANALES_CHROOT rsem-bam2wig ${sample_name}.transcript.sorted.bam ${sample_name}.transcript.wig $sample_name
+$CANALES_CHROOT rsem-plot-model $sample_name ${sample_name}.models.pdf
+
